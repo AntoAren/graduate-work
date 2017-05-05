c7e904d8a2062fbd6249039f3bdf8dd819478b23
Anton Zakharenkov <tosha.zakharenkou@yandex.ru>
2017-05-05 22:49:16 +0300

    add Record entity

---
+++ rest-services/model/src/main/java/by/bsu/zakharankou/restservices/model/record/Record.java
@@ -0,0 +1,83 @@
+package by.bsu.zakharankou.restservices.model.record;
+
+import by.bsu.zakharankou.restservices.model.answer.Answer;
+import by.bsu.zakharankou.restservices.model.question.Question;
+import by.bsu.zakharankou.restservices.model.test.Test;
+import by.bsu.zakharankou.restservices.model.user.User;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Record {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "test_id")
+    private Test test;
+
+    @ManyToOne
+    @JoinColumn(name = "question_id")
+    private Question question;
+
+    @ManyToOne
+    @JoinColumn(name = "answer_id")
+    private Answer answer;
+
+    private Date creationDate = new Date();
+
+    @ManyToOne
+    @JoinColumn(name = "username")
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Test getTest() {
+        return test;
+    }
+
+    public void setTest(Test test) {
+        this.test = test;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public Answer getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Answer answer) {
+        this.answer = answer;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
